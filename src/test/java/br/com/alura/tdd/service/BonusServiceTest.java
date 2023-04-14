83b948d2ad97d497bb33fa666e18548271af3b65
Daiana Michels <daianaem.12@gmail.com>
2023-04-13 22:23:35 -0300

    adds test for high salary scenario

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,5 +1,20 @@
 package br.com.alura.tdd.service;
 
+import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BonusServiceTest {
 
+    @Test
+    public void deveriaRetornarZeroQuandoOSalarioForAlto(){
+        BonusService bonusService = new BonusService();
+        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("Cristal", LocalDate.now(), new BigDecimal(12000.00)));
+
+        assertEquals(new BigDecimal("0.00"), bonus);
+    }
 }
