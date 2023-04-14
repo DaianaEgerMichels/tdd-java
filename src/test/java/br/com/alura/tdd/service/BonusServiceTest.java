8ed7d4a73a80baa9bd8fe86d3b2f15442ce0d0bb
Daiana Michels <daianaem.12@gmail.com>
2023-04-13 22:29:20 -0300

    adds test for ten thousand salary scenario

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -25,4 +25,12 @@ public class BonusServiceTest {
 
         assertEquals(new BigDecimal("200.00"), bonus);
     }
+
+    @Test
+    public void deveriaRetornarMilQuandoOSalarioForExatamenteDezMil(){
+        BonusService bonusService = new BonusService();
+        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("Karen", LocalDate.now(), new BigDecimal(10000.00)));
+
+        assertEquals(new BigDecimal("1000.00"), bonus);
+    }
 }
