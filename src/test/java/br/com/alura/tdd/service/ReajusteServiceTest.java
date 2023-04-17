e6293c261d6fb3f5c67a80861c05c6ddeaf210dd
Daiana Michels <daianaem.12@gmail.com>
2023-04-17 20:21:38 -0300

    TDD: adds test for creation of readjustment method

---
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -0,0 +1,29 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReajusteServiceTest {
+
+
+    @Test
+    public void reajusteDeveriaSerDeTresPorcentoQuandoDesempenhoForADesejar() {
+        // classe service para gerenciar o reajuste do salário
+        ReajusteService service = new ReajusteService();
+
+        // funcionario criado para validar o método de reajuste
+        Funcionario funcionario = new Funcionario("Lara", LocalDate.now(), new BigDecimal("1000.00"));
+
+        // aplica o método de reajuste
+        service.concederReajuste(funcionario, Desempenho.A_DESEJAR);
+
+        assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
+
+
+    }
+}
